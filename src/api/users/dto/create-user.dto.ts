aa604cf0574b544c3b05424a10ef339ecb4f1628
SelectPerson <vovalutsk961@gmail.com>
2022-12-13 19:39:03 +0200

    end task v2

--- src/api/users/dto/create-user.dto.ts
+++ src/api/users/dto/create-user.dto.ts
@@ -1,10 +1,15 @@
 import { iUser, userRoleTypes } from '../../../types/User/User';
+import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto implements iUser {
+  @IsEmail()
   readonly email: string;
   readonly id: number;
+  @IsString()
   readonly name: string;
+  @IsString()
   readonly phone: string;
   readonly role: userRoleTypes;
+  @IsString()
   readonly password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from './pipes/validation.pipe';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
 
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
   });
